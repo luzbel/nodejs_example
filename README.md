7282fee59fa8fd9d145b010b99b341108e60e2db
ian eyberg <ian@deferpanic.com>
2016-10-25 10:01:11 -0700

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+# nodejs example
+This is a simple nodejs example running on the deferpanic unikernel platform.
+
+It includes an example of installing dependencies using deferpanic.yml the configuration file.
---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.end('Node JS and express running on a unikernel! Requested url: ' + req.url);
+});
+
+var port =3000;
+app.listen(port);
+
+module.exports = app;
