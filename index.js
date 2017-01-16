041dd022bdc4f6c1d4efa3df4fc833fe7328877e
luzbel <luzbel@users.noreply.github.com>
2017-01-16 16:39:46 +0100

    Añado fecha

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.end('Node JS and express running on a unikernel! Requested url: ' + req.url);
+    res.end('Node JS and express running on a unikernel! Requested url: ' + req.url+' Timestamp:'+Date.now());
 });
 
 var port =3000;
